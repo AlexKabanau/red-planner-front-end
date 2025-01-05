011bd432042d55529dd92965ba652c95a22dd9f4
Alexandr Kabanau <Azraill1989@gmail.com>
2025-01-05 16:23:54 +0300

    feat: dnd tasks and date groups

---
+++ src/app/tasks/TasksView.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React from 'react'
+
+import { ListView } from './list-view/ListView'
+
+export function TasksView() {
+	//TODO: add kanban view
+	//TODO: Suspense
+	return <ListView />
+}
