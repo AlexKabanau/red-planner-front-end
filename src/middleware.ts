da80851d89a9347829451f244a686b9a73c549fb
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-21 15:46:51 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,31 +3,35 @@ import { NextRequest, NextResponse } from 'next/server'
 import { DASHBOARD_PAGES } from './config/pages-url.config'
 import { EnumTokens } from './services/auth-tokent.service'
 
-export async function middleware(request: NextRequest, response: NextResponse) {
+export async function middleware(request: NextRequest) {
 	const { url, cookies } = request
 
 	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
-
 	const isAuthPage = url.includes('/auth')
-	debugger
-	console.log(refreshToken)
-	console.log(isAuthPage)
+
+	// Логирование
+	console.log('Refresh Token:', refreshToken)
+	console.log('Is Auth Page:', isAuthPage)
+
 	if (isAuthPage && refreshToken) {
-		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
+		console.log('Redirecting to dashboard...')
+		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, request.url))
 	}
 
 	if (isAuthPage) {
+		console.log('User is on auth page without token. Proceeding...')
 		return NextResponse.next()
 	}
 
 	if (!refreshToken) {
+		console.log('No token found. Redirecting to auth...')
 		return NextResponse.redirect(new URL('/auth', request.url))
 	}
 
+	console.log('Token is valid. Proceeding...')
 	return NextResponse.next()
 }
 
 export const config = {
 	matcher: ['/auth/:path*', '/i/:path*']
-	// matcher: ['/i/:path*', '/auth/:path']
 }
