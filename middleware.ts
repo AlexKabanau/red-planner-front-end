fb29a32e11a3810eb041f180989a2bdf7647a1bc
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-22 14:23:52 +0300

    fix: fix routes

--- middleware.ts
+++ middleware.ts
@@ -1,44 +1,49 @@
+// import { NextRequest, NextResponse } from 'next/server'
 import { NextRequest, NextResponse } from 'next/server'
 
-import { DASHBOARD_PAGES } from './config/pages-url.config'
-import { EnumTokens } from './services/auth-tokent.service'
+import { DASHBOARD_PAGES } from '@/config/pages-url.config'
+
+import { EnumTokens } from '@/services/auth-tokent.service'
+
+// import { DASHBOARD_PAGES } from './src/config/pages-url.config'
+// import { EnumTokens } from './src/services/auth-tokent.service'
 
 export async function middleware(request: NextRequest) {
-	const refreshToken = request.cookies.get('refreshToken')?.value || 'undefined'
-	console.log('Middleware executed:', { url: request.url, refreshToken })
+	// const refreshToken = request.cookies.get('refreshToken')?.value || 'undefined'
+	// console.log('Middleware executed:', { url: request.url, refreshToken })
 
-	return NextResponse.next()
-
-	// const { url, cookies } = request
-
-	// const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
-	// const isAuthPage = url.includes('/auth')
-
-	// // Логирование для диагностики
-	// console.log('Refresh Token:', refreshToken)
-	// console.log('Is Auth Page:', isAuthPage)
-	// console.log('Request URL:', url)
-
-	// // Редирект, если пользователь на /auth и имеет токен
-	// if (isAuthPage && refreshToken) {
-	// 	console.log('Redirecting to Dashboard:', DASHBOARD_PAGES.HOME)
-	// 	return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
-	// }
-
-	// // Продолжить выполнение, если пользователь на /auth без токена
-	// if (isAuthPage) {
-	// 	console.log('User is on auth page without token')
-	// 	return NextResponse.next()
-	// }
-
-	// // Редирект на /auth, если токена нет
-	// if (!refreshToken) {
-	// 	console.log('No token found, redirecting to /auth')
-	// 	return NextResponse.redirect(new URL('/auth', request.url))
-	// }
-
-	// // Продолжить выполнение для других маршрутов
 	// return NextResponse.next()
+
+	const { url, cookies } = request
+
+	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+	const isAuthPage = url.includes('/auth')
+
+	// Логирование для диагностики
+	console.log('Refresh Token:', refreshToken)
+	console.log('Is Auth Page:', isAuthPage)
+	console.log('Request URL:', url)
+
+	// Редирект, если пользователь на /auth и имеет токен
+	if (isAuthPage && refreshToken) {
+		console.log('Redirecting to Dashboard:', DASHBOARD_PAGES.HOME)
+		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
+	}
+
+	// Продолжить выполнение, если пользователь на /auth без токена
+	if (isAuthPage) {
+		console.log('User is on auth page without token')
+		return NextResponse.next()
+	}
+
+	// Редирект на /auth, если токена нет
+	if (!refreshToken) {
+		console.log('No token found, redirecting to /auth')
+		return NextResponse.redirect(new URL('/auth', request.url))
+	}
+
+	// Продолжить выполнение для других маршрутов
+	return NextResponse.next()
 }
 
 export const config = {
