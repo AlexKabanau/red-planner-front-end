9e9649324c9d06f426b0c6dcd7a09b536c089a34
Alexandr Kabanau <azraill1989@gmail.com>
2025-01-22 12:54:56 +0300

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,26 +9,30 @@ export async function middleware(request: NextRequest) {
 	const refreshToken = cookies.get(EnumTokens.REFRESH_TOKEN)?.value
 	const isAuthPage = url.includes('/auth')
 
-	// Логирование
+	// Логирование для диагностики
 	console.log('Refresh Token:', refreshToken)
 	console.log('Is Auth Page:', isAuthPage)
+	console.log('Request URL:', url)
 
+	// Редирект, если пользователь на /auth и имеет токен
 	if (isAuthPage && refreshToken) {
-		console.log('Redirecting to dashboard...')
-		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, request.url))
+		console.log('Redirecting to Dashboard:', DASHBOARD_PAGES.HOME)
+		return NextResponse.redirect(new URL(DASHBOARD_PAGES.HOME, url))
 	}
 
+	// Продолжить выполнение, если пользователь на /auth без токена
 	if (isAuthPage) {
-		console.log('User is on auth page without token. Proceeding...')
+		console.log('User is on auth page without token')
 		return NextResponse.next()
 	}
 
+	// Редирект на /auth, если токена нет
 	if (!refreshToken) {
-		console.log('No token found. Redirecting to auth...')
+		console.log('No token found, redirecting to /auth')
 		return NextResponse.redirect(new URL('/auth', request.url))
 	}
 
-	console.log('Token is valid. Proceeding...')
+	// Продолжить выполнение для других маршрутов
 	return NextResponse.next()
 }
 
